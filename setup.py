90358bb29a651b03c64adb9ddcb08c762c7aea23
Wesley Kerfoot <wjak56@gmail.com>
2019-05-20 10:50:52 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="delete-facebook-posts",
-    version="1.0.0",
+    version="1.0.1",
     author="Wesley Kerfoot",
     author_email="wes@wesk.tech",
     description="A Selenium Script to Delete Facebook Posts",
